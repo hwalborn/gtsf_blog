545d854066a1aeee08236353eaadc495b9e168cf
hwalborn <hwalborn@gmail.com>
2018-10-13 14:05:20 -0400

    ready for first part

--- gtsf_blog.rb
+++ gtsf_blog.rb
@@ -1,1 +1,9 @@
-data = Net::HTTP.get(URI.parse("http://datamine.mta.info/mta_esi.php?key=#{key}&feed_id=1"))
+require_relative 'api_key.rb'
+require 'protobuf'
+require 'google/transit/gtfs-realtime.pb'
+require 'net/http'
+require 'uri'
+require 'byebug'
+
+data = Net::HTTP.get(URI.parse("http://datamine.mta.info/mta_esi.php?key=#{ApiKey::KEY}"))
+feed_list = Transit_realtime::FeedMessage.decode(data).entity
